943d3486c4b8ad7c5bbc4c0c4ce360c1583d8437
Beomil KIM <prixe2000@gmail.com>
2020-11-06 14:36:36 +0900

    아파트 시세 조건 CSV

--- apt.py
+++ apt.py
@@ -5,12 +5,9 @@ new_list=[]
 
 for i in apt:
     try:
-        if  i[5] > 120 and i[-4] < 80000 and re.match("강원",i[0]):
+        if  i[5] > 120 and i[-4] > 20000 and re.search("전주",i[0]):
             new_list.append([i[0],i[4],i[-4]])
     except:
         pass
 
 usecsv.writecsv('over120_over3000.csv',new_list)
-
-#i[5]>120 and i[-4]<80000 and
-#re.match("강원",i[0]):
