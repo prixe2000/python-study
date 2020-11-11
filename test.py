79a5434f048cc1b7fff0acee2299a6b842fbae39
Beomil KIM <prixe2000@gmail.com>
2020-11-11 14:41:28 +0900

    웹크롤링(기사) 설치파일 만들기

---
+++ test.py
@@ -0,0 +1,23 @@
+import os, re, usecsv
+import requests
+import urllib.request as ur
+from bs4 import BeautifulSoup as bs
+
+news='http://news.daum.net/'
+soup=bs(ur.urlopen(news).read(),'html.parser')
+
+f=open('article_total.txt','w')
+for i in soup.find_all('div',{"class":"relate_thumb"}):
+    try:
+        f.write(i.text + '\n')
+        f.write(i.find_all('a')[0].get('href')+'\n')
+        soup2=bs(ur.urlopen(i.find_all('a')[0].get('href')).read(),'html.parser')
+        for j in soup2.find_all('p'):
+            f.write(j.text)
+    except:
+        pass
+f.close()
+
+#exe 파일 만들기 명령어 프롬프트 명령 
+#pip install pyinstaller
+#pyinstaller --onefile test.py
