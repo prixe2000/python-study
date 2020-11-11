0dbac6258c25b9719876601f97f99ff26e8a041d
Beomil KIM <prixe2000@gmail.com>
2020-11-11 17:22:12 +0900

    뉴스기사 스크래핑 프로그램 배포판

--- test.py
+++ test.py
@@ -2,15 +2,17 @@ import os, re, usecsv
 import requests
 import urllib.request as ur
 from bs4 import BeautifulSoup as bs
+import datetime
 
 news='http://news.daum.net/'
 soup=bs(ur.urlopen(news).read(),'html.parser')
+nowDate=datetime.datetime.now()
 
-f=open('article_total.txt','w')
+f=open(nowDate.strftime("%Y-%m-%d_%H%M")+".txt","w")
 for i in soup.find_all('div',{"class":"relate_thumb"}):
     try:
         f.write(i.text + '\n')
-        f.write(i.find_all('a')[0].get('href')+'\n')
+        #f.write(i.find_all('a')[0].get('href')+'\n') #하이퍼링크도 넣기
         soup2=bs(ur.urlopen(i.find_all('a')[0].get('href')).read(),'html.parser')
         for j in soup2.find_all('p'):
             f.write(j.text)
